aff06506fd943bf6b4634d23ba87021f550205f8
krishnakalyana <krishnakalyanax@gmail.com>
2020-06-08 12:23:13 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+
+const path = require('path')
+const dotenv = require('dotenv')
+const methodOverride =require('method-override')
+const session = require('express-session')
+const flash =require('connect-flash')
+const mongoose = require('mongoose')
+const employeeRoutes = require('./routes/employee')
+const bodyParser = require('body-parser')
+
+dotenv.config({path:'./config.env'})
+mongoose.connect(process.env.DATABASE_LOCAL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useCreateIndex:true
+
+})
+app.use(bodyParser.urlencoded({extended:true}))
+app.set('views',path.join(__dirname,'views'))
+app.set('view engine','ejs')
+app.use(express.static('public'))
+app.use(methodOverride('_method'))
+app.use(session({
+    secret : "thisIsNodeJsSecret",
+    resave: true,
+    saveUninitialized:true,
+}))
+app.use(flash())
+app.use((req,res,next)=>{
+    res.locals.success_msg =req.flash(('success_msg'))
+    res.locals.error_msg =req.flash(('error_msg'))
+    next()
+})
+app.use(employeeRoutes)
+
+const port = process.env.PORT
+app.listen(port,()=>{
+ console.log(`server started on port ${port}.`)
+})
---
+++ routes/employee.js
@@ -0,0 +1,94 @@
+const express = require('express')
+const router  = express.Router()
+const Employee =require('../models/employee')
+
+router.get('/',(req,res)=>{
+    Employee.find({})
+        .then(employees=>{
+            res.render('index',{employees:employees})
+        })
+        .catch(err=>{
+            req.flash('error_msg','ERROR:'+err)
+            res.redirect('/') 
+        })
+    
+})
+
+router.get('/employee/new',(req,res)=>{
+    res.render('new')
+})
+router.get('/employee/search',(req,res)=>{
+    res.render('search',{employee:""})
+})
+router.get('/employee',(req,res)=>{
+    let searchQuery = {name:req.query.name}
+    Employee.findOne(searchQuery)
+        .then(employee=>{
+            res.render('search',{employee:employee})
+        })
+        .catch(err=>{
+            req.flash('error_msg','ERROR:'+err)
+            res.redirect('/')
+        })
+})
+router.get('/edit/:id',(req,res)=>{
+    let searchQuery = {_id:req.params.id}
+    Employee.findOne(searchQuery)
+        .then(employee=>{
+            res.render('edit',{employee:employee})
+        })
+        .catch(err=>{
+            req.flash('error_msg','ERROR:'+err)
+            res.redirect('/')
+        })
+})
+
+router.put('/edit/:id',(req,res)=>{
+    let searchQuery = {_id:req.params.id}
+    Employee.updateOne(searchQuery,{$set:{
+        name:req.body.name,
+        designation:req.body.designation,
+        salary:req.body.salary,
+    }})
+    .then(employee=>{
+        req.flash('success_msg','Employee Updated Successfully.')
+        res.redirect('/')
+    })
+    .catch(err=>{
+        req.flash('error_msg','ERROR:'+err)
+        res.redirect('/')
+    })
+})
+
+
+
+router.post('/employee/new',(req,res)=>{
+    let newEmployee = {
+        name:req.body.name,
+        designation:req.body.designation,
+        salary:req.body.salary
+    }
+    Employee.create(newEmployee)
+        .then(employee=>{
+            req.flash('success_msg','Employee Added Successfully.')
+            res.redirect('/')
+        })
+        .catch(err=>{
+            req.flash('error_msg','ERROR:'+err)
+            res.redirect('/')
+        })
+})
+
+router.delete('/delete/:id',(req,res)=>{
+    let searchQuery = {_id:req.params.id,}
+    Employee.deleteOne(searchQuery)
+        .then(employee=>{
+            req.flash('success_msg','Employee deleted Successfully.')
+            res.redirect('/')
+        })
+        .catch(err=>{
+            req.flash('error_msg','ERROR:'+err)
+            res.redirect('/')        
+        })
+})
+module.exports = router
